9113812b7b58c6d6c7fcc7c9a7256242166ab7f1
Ankit Kumar <ankit.kumar@rea-group.com>
2020-05-06 11:10:15 +0800

    added babel preset

--- webpack-transpiling/webpack.config.js
+++ webpack-transpiling/webpack.config.js
@@ -1,9 +1,17 @@
+var webpack = require("webpack");
+
 module.exports = {
-	entry: './script.js',
-	output: {filename: 'bundle.js'},
-	module: {
-		loaders: [
-			{test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/}
-		]
-	}
+  entry: "./script.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
